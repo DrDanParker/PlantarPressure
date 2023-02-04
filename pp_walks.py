a741f557fdd464628e5d609cf2d533e627c06653
Dan Parker <32143583+DrDanParker@users.noreply.github.com>
2023-02-04 08:48:07 +0000

    Update pp_walks.py

--- pp_walks.py
+++ pp_walks.py
@@ -12,6 +12,7 @@
 #Dependencies: 
 import xarray as xr
 from matplotlib import pyplot	as plt
+from scipy.signal import find_peaks
 
 #Repo Functions:
 import pp_import as p_in
@@ -25,8 +26,11 @@ arrays = p_in.batch_load(fdir)
 for key in arrays:
     P_Dataset = arrays[key].to_array()
     PP_Curve = P_Dataset.max(dim=['l','w'])
+    peaks = find_peaks(PP_Curve, height=200, threshold=5, distance=40)
+    
     plt.figure()
     PP_Curve.plot()
+    plt.plot(peaks[0],peaks[1]['peak_heights'], '*')
 
 plt.show()
 
